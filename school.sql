1172cb04471af2dae8af19c209ffedf192e9ade8
TheEvilSocks <theevilsocks@gmail.com>
2016-12-15 10:12:10 +0100

    merge or w/e

--- school.sql
+++ school.sql
@@ -3,11 +3,11 @@ CREATE DATABASE school;
 
 CREATE TABLE STUDENTS
 (
-ID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-BSN int(11),
-Enrollment_Year int(4),
-Name varchar(255),
-Surname varchar(255)
+	ID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	BSN int(11),
+	Enrollment_Year int(4),
+	Name varchar(255),
+	Surname varchar(255)
 );
 
 
@@ -26,12 +26,12 @@ CREATE TABLE TEACHERS
 
 CREATE TABLE COURSES
 (
-CODE varchar(255) NOT NULL PRIMARY KEY,
-Name varchar(255),
-STID int(11),
-StudyPoints int(2),
-FOREIGN KEY (CODE) REFERENCES TEACHERS(BSN),
-FOREIGN KEY (STID) REFERENCES STUDENTS(ID)
+	CODE varchar(255) NOT NULL PRIMARY KEY,
+	Name varchar(255),
+	STID int(11),
+	StudyPoints int(2),
+	FOREIGN KEY (CODE) REFERENCES TEACHERS(BSN),
+	FOREIGN KEY (STID) REFERENCES STUDENTS(ID)
 );
 
 
@@ -39,30 +39,30 @@ FOREIGN KEY (STID) REFERENCES STUDENTS(ID)
 
 CREATE TABLE ASSIGNMENTS
 (
-CODE varchar(255) NOT NULL PRIMARY KEY,
-Week int(2),
-Year int(4),
-AssesmentType int(1),
-STID int(11),
-IsValid boolean,
-FOREIGN KEY (CODE) REFERENCES COURSES(CODE),
-FOREIGN KEY (CODE) REFERENCES TEACHERS(BSN),
-FOREIGN KEY (STID) REFERENCES STUDENTS(ID)
+	CODE varchar(255) NOT NULL PRIMARY KEY,
+	Week int(2),
+	Year int(4),
+	AssesmentType int(1),
+	STID int(11),
+	IsValid boolean,
+	FOREIGN KEY (CODE) REFERENCES COURSES(CODE),
+	FOREIGN KEY (CODE) REFERENCES TEACHERS(BSN),
+	FOREIGN KEY (STID) REFERENCES STUDENTS(ID)
 );
 
 
 
 
 INSERT INTO ASSIGNMENTS
-VALUES (‘Final Maymay assignment’, 34, 2016, ‘summative’);
+VALUES ('Final Maymay assignment', 34, 2016, 'summative');
 
 
 INSERT INTO COURSES
-VALUES (‘maymayCourse’, ‘Learn 2 meme’, 6);
+VALUES ('maymayCourse', 'Learn 2 meme', 6);
 
 
 INSERT INTO STUDENTS
-VALUES (‘589761961’, 2016, ‘John’, ‘Smith’);
+VALUES ('589761961', 2016, 'John', 'Smith');
 
 
 -- get student 👌
