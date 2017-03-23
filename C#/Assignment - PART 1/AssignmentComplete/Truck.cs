c2f59872156ce3296a33c0225bd978999965e8a9
TheEvilSocks <theevilsocks@gmail.com>
2017-03-23 16:59:00 +0100

    Update Truck, Add OreContainer

--- C#/Assignment - PART 1/AssignmentComplete/Truck.cs
+++ C#/Assignment - PART 1/AssignmentComplete/Truck.cs
@@ -25,51 +25,69 @@ namespace AssignmentComplete
             IContainer Container = container;
             this.position = position;
             this.velocity = velocity;
-
         }
 
-        class DrawTruck : IAction
+        public void Draw(SpriteBatch spriteBatch)
         {
-
-            Mine mine;
-            public DrawTruck(Mine mine)
-            {
-                this.mine = mine;
-            }
-            public void Run()
-            {
-                mine.ProductsToShip.Add(CreateOreBox(mine.Position + new Vector2(-80, 40 + -30 * mine.ProductsToShip.Count)));
-            }
-            Ore CreateOreBox(Vector2 position)
-            {
-                var box = new Ore(100, mine.OreBox());
-                box.Position = position;
-                return box;
-            }
+            spriteBatch.Draw(texture, Position, Color.Magenta);
         }
 
-        Texture2D volvo;
-        List<IStateMachine> processes;
-        Ore orebox;
-        bool driving = false;
-
-        public void StartEngine()
+        public void Update(float f)
         {
-
+            position += Velocity;
         }
+
         public void AddContainer(IContainer container)
         {
 
         }
 
-        public void Draw(SpriteBatch spriteBatch)
+        public void StartEngine()
         {
-            spriteBatch.Draw(volvo, Position, Color.Blue);
+
         }
 
-        public void Update(float dt)
+
+    }
+
+    class OreContainer : ITruck
+    {
+
+        Texture2D texture;
+        public IContainer Container { get { return Container; } }
+        public Vector2 position;
+        public Vector2 Position { get { return position; } }
+        public Vector2 velocity;
+        public Vector2 Velocity { get { return velocity; } }
+
+
+
+        public OreContainer(Texture2D texture, IContainer container, Vector2 position, Vector2 velocity)
         {
-            position += velocity * dt;
+            this.texture = texture;
+            IContainer Container = container;
+            this.position = position;
+            this.velocity = velocity;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, Position, Color.Magenta);
+        }
+
+        public void Update(float f)
+        {
+            position += Velocity;
+        }
+
+        public void AddContainer(IContainer container)
+        {
+
+        }
+
+        public void StartEngine()
+        {
+
         }
     }
 }
