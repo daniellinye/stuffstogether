72a05f29a0194e846b6704d8088742eb64a5d9d9
TheEvilSocks <theevilsocks@gmail.com>
2017-03-24 13:49:09 +0100

    shit ass git client fucking hell ey

--- C#/Assignment - PART 1/AssignmentComplete/Factory.cs
+++ C#/Assignment - PART 1/AssignmentComplete/Factory.cs
@@ -64,9 +64,8 @@ namespace AssignmentComplete
         public ITruck GetReadyTruck()
         {
             if (isTruckReady)
-            {
                 return new Truck(truckTexture, null, position + new Vector2(76, 32), new Vector2(2, 0));
-            }
+            
             return null;
         }
         public ITruck GetReadyOreContainer()
@@ -110,9 +109,8 @@ namespace AssignmentComplete
         public void Update(float dt)
         {
             foreach (var process in processes)
-            {
                 process.Update(dt);
-            }
+            
         }
 
     }
@@ -120,75 +118,70 @@ namespace AssignmentComplete
     class Ikea : IFactory
     {
 
-        class AddOreBoxToMine : IAction
+        class AddOreBoxToIkea : IAction
         {
             Ikea ikea;
-            public void CreateProduct(Ikea ikea)
+            public AddOreBoxToIkea(Ikea ikea)
             {
                 this.ikea = ikea;
             }
             public void Run()
             {
-                ikea.ProductsToShip.Add(CreateOreBox(ikea.Position + new Vector2(-80, 40 + -30 * ikea.ProductsToShip.Count)));
-                if (ikea.productsToShip.Count == 4)
-                {
-                    ikea.productsToShip = ikea.productsToShip.GetRange(0, ikea.productsToShip.Count - 4);
+                if (ikea.productsToShip.Count() == 2)
                     ikea.isTruckReady = true;
-                }
+                if (ikea.productsToShip.Count() == 3)
+                    ikea.productsToShip.Clear();
+
+                ikea.ProductsToShip.Add(CreateOreBox(ikea.Position + new Vector2(-80, 40 + -30 * ikea.ProductsToShip.Count)));
             }
-
-
-            //TODO: Make a product class
             Ore CreateOreBox(Vector2 position)
             {
-                var box = new Ore(100, ikea.productBox);
+                var box = new Ore(100, ikea.oreBox);
                 box.Position = position;
                 return box;
             }
         }
 
-        Texture2D ikea, product_box, product_container, truckTexture;
+        Texture2D ikea, oreContainer, oreBox, truckTexture;
         List<IStateMachine> processes;
         ITruck waitingTruck;
         bool isTruckReady = false;
         Vector2 position;
         List<IContainer> productsToShip;
 
-        public Ikea(Vector2 position, Texture2D truck_texture, Texture2D ikea, Texture2D product_box, Texture2D product_container)
+        public Ikea(Vector2 position, Texture2D truck_texture, Texture2D ikea, Texture2D ore_box, Texture2D ore_container)
         {
             processes = new List<IStateMachine>();
             ProductsToShip = new List<IContainer>();
             this.ikea = ikea;
             this.truckTexture = truck_texture;
-            this.product_box = product_box;
-            this.product_container = product_container;
+            this.oreContainer = ore_container;
+            this.oreBox = ore_box;
             this.position = position;
+            waitingTruck = new Truck(truckTexture, null, position + new Vector2(-64, 32), new Vector2(0, 0));
 
+
+            processes.Add(
+              new Repeat(new Seq(new Timer(1.0f),
+                  new Call(new AddOreBoxToIkea(this)))));
 
             processes.Add(new Timer(1.0f));
 
-            processes.Add(new Timer(1.0f));
-
-        }
-
-        public Texture2D productBox()
-        {
-            return this.product_box;
         }
 
         public ITruck GetReadyTruck()
         {
             if (isTruckReady)
-            {
-                return new OreContainer(truckTexture, null, position + new Vector2(64, 32), new Vector2(2, 0));
-            }
+                return new Truck(truckTexture, null, position + new Vector2(-76, 32), new Vector2(-2, 0));
+            
             return null;
         }
         public ITruck GetReadyOreContainer()
         {
             if (isTruckReady)
             {
-                return new Truck(truckTexture, null, position + new Vector2(64, 32), new Vector2(2, 0));
+                isTruckReady = false;
+                return new OreContainer(oreContainer, null, position + new Vector2(-64, 20), new Vector2(-2, 0));
             }
             return null;
         }
@@ -214,17 +207,18 @@ namespace AssignmentComplete
         public void Draw(SpriteBatch spriteBatch)
         {
             foreach (var cart in ProductsToShip)
-            {
                 cart.Draw(spriteBatch);
-            }
+
+            if (productsToShip.Count() < 3)
+                waitingTruck.Draw(spriteBatch);
+
             spriteBatch.Draw(ikea, Position, Color.White);
         }
         public void Update(float dt)
         {
             foreach (var process in processes)
-            {
                 process.Update(dt);
-            }
+
         }
 
     }
